a087ec472af3bd9b87dc9800b7e83dba38a7dd2d
Haris Riyoni <1214047@std.ulbi.ac.id>
2025-12-03 13:10:26 +0700

    rest api fix

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"rest-learn/app"
+	"rest-learn/controller"
+	"rest-learn/helper"
+	"rest-learn/reporitory"
+	"rest-learn/service"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/go-playground/validator"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	db := app.NewDB()
+	validate := validator.New()
+	categoryRepository := reporitory.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryService)
+
+	router := httprouter.New()
+
+	router.GET("/api/categories", categoryController.FindAll)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.POST("/api/categories", categoryController.Create)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+
+	helper.PanicIfError(err)
+}
--- reporitory/category_repository_impl.go
+++ reporitory/category_repository_impl.go
@@ -11,8 +11,12 @@ import (
 type CategoryRepositoryImpl struct {
 }
 
+func NewCategoryRepository() CategoryRepository {
+	return &CategoryRepositoryImpl{}
+}
+
 func (c *CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
-	SQL := "insert into customer(name values (?)"
+	SQL := "insert into category(name) values (?)"
 	result, err := tx.ExecContext(ctx, SQL, category.Name)
 	helper.PanicIfError(err)
 
@@ -31,25 +35,24 @@ func (c *CategoryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, categor
 	return category
 }
 
-func (c *CategoryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+func (c *CategoryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) {
 	SQL := "delete from category where id = ?"
 	_, err := tx.ExecContext(ctx, SQL, category.Id)
 	helper.PanicIfError(err)
-
-	// return category
 }
 
 func (c *CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error) {
 	SQL := "select id, name from category where id = ?"
 	rows, err := tx.QueryContext(ctx, SQL, categoryId)
 	helper.PanicIfError(err)
+	defer rows.Close()
 	category := domain.Category{}
 	if rows.Next() {
 		err := rows.Scan(&category.Id, &category.Name)
 		helper.PanicIfError(err)
 		return category, nil
 	} else {
-		return category, errors.New("category is not found!")
+		return category, errors.New("category is not found")
 	}
 }
 
@@ -57,6 +60,7 @@ func (c *CategoryRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) []doma
 	SQL := "select id, name from category"
 	rows, err := tx.QueryContext(ctx, SQL)
 	helper.PanicIfError(err)
+	defer rows.Close()
 
 	var categories []domain.Category
 	for rows.Next() {
